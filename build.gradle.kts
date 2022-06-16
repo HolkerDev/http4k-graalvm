6a7cbd3ba9277b36619838905f71baf95ba9ba88
Mykola Penkovskyi <36474981+HolkerDev@users.noreply.github.com>
2022-06-16 20:24:26 +0200

    awdawd

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.7.0-RC"
+    application
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+}
+
+group = "org.example"
+version = ""
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation("org.http4k:http4k-serverless-lambda-runtime:4.27.0.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClass.set("MainKt")
+}
+
+tasks.withType<ShadowJar> {
+    manifest.attributes("Main-Class" to "MainKt")
+    archiveBaseName.set(project.name)
+    mergeServiceFiles()
+}
