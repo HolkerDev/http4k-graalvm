c912c16e4f0197ed1f1902213d71c2100dfe755d
Mykola Penkovskyi <36474981+HolkerDev@users.noreply.github.com>
2022-06-19 11:25:35 +0200

    fix type of lambda

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,7 +4,7 @@ import org.http4k.core.Status.Companion.OK
 import org.http4k.routing.bind
 import org.http4k.routing.path
 import org.http4k.routing.routes
-import org.http4k.serverless.ApiGatewayV1FnLoader
+import org.http4k.serverless.ApiGatewayRestFnLoader
 import org.http4k.serverless.AwsLambdaRuntime
 import org.http4k.serverless.asServer
 
@@ -19,5 +19,5 @@ val http4kApp = routes(
 
 fun main() {
     print("test")
-    ApiGatewayV1FnLoader(http4kApp).asServer(AwsLambdaRuntime()).start()
+    ApiGatewayRestFnLoader(http4kApp).asServer(AwsLambdaRuntime()).start()
 }
