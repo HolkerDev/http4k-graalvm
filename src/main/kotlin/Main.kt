2864d2d87fe4ee4e5939ccdb6945a7d01b27ade4
Mykola Penkovskyi <36474981+HolkerDev@users.noreply.github.com>
2022-06-19 15:17:06 +0200

    add json endpoint for test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,9 @@
+import org.http4k.core.Body
 import org.http4k.core.Method
 import org.http4k.core.Response
 import org.http4k.core.Status.Companion.OK
+import org.http4k.core.with
+import org.http4k.format.AwsLambdaMoshi.auto
 import org.http4k.routing.bind
 import org.http4k.routing.path
 import org.http4k.routing.routes
@@ -8,11 +11,16 @@ import org.http4k.serverless.ApiGatewayRestFnLoader
 import org.http4k.serverless.AwsLambdaRuntime
 import org.http4k.serverless.asServer
 
+data class Person(val name: String)
+
 val http4kApp = routes(
     "/echo/{message:.*}" bind Method.GET to {
         Response(OK).body(
             it.path("message") ?: "(nothing to echo, use /echo/<message>)"
         )
+    },
+    "/json" bind Method.GET to { req->
+        Response(OK).with(Body.auto<Person>().toLens() of Person("my test name for test function"))
     },
     "/" bind Method.GET to { Response(OK).body("ok") }
 )
