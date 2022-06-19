d742f4e60012bafabdede9bbbc973134d2335c0b
Mykola Penkovskyi <36474981+HolkerDev@users.noreply.github.com>
2022-06-19 15:57:41 +0200

    adjust code for graalvm

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     kotlin("jvm") version "1.7.0-RC"
     application
     id("com.github.johnrengelman.shadow") version "7.1.2"
+    kotlin("plugin.serialization") version "1.7.0-RC"
 }
 
 group = "org.example"
@@ -17,6 +18,10 @@ repositories {
 dependencies {
     testImplementation(kotlin("test"))
     implementation("org.http4k:http4k-serverless-lambda-runtime:4.27.0.0")
+    // implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.3")
+    // implementation("org.htt4k:http4k-format-kotlinx-serialization:4.27.0.0")
+    implementation("org.http4k:http4k-format-kotlinx-serialization:4.25.14.0")
+
 }
 
 tasks.test {
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,18 @@
+import kotlinx.serialization.Serializable
 import org.http4k.core.Body
 import org.http4k.core.Method
 import org.http4k.core.Response
 import org.http4k.core.Status.Companion.OK
 import org.http4k.core.with
-import org.http4k.format.AwsLambdaMoshi.auto
 import org.http4k.routing.bind
 import org.http4k.routing.path
 import org.http4k.routing.routes
 import org.http4k.serverless.ApiGatewayRestFnLoader
 import org.http4k.serverless.AwsLambdaRuntime
 import org.http4k.serverless.asServer
+import org.http4k.format.KotlinxSerialization.auto
 
+@Serializable
 data class Person(val name: String)
 
 val http4kApp = routes(
